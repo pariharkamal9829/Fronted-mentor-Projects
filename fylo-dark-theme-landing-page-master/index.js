4aea9367c25199083c58f64ff956ca309ed0887b
Kamlesh Parihar <pariharkamal9829@gmail.com>
2023-11-06 08:02:04 +0530

    navbar upadate 2

--- fylo-dark-theme-landing-page-master/index.js
+++ fylo-dark-theme-landing-page-master/index.js
@@ -1,9 +1,12 @@
 const hamBurger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const body = document.body;
+const otherSections = document.querySelectorAll(".section-to-move");
 
-hamBurger.addEventListener("click", () =>{
+hamBurger.addEventListener("click", () => {
+  hamBurger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
 
-    hamBurger.classList.toggle("active");
-    navMenu.classList.toggle("active");
 
-})
+
